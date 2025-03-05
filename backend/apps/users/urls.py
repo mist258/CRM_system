2d42ed779745c88a8d61552286898259c68f1545
mist256 <natalia.kolchuk@gmail.com>
2025-03-05 07:36:33 +0200

    fix

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import ListCreateManagerView, ManagerBanView, ManagerUnbanView
 
 urlpatterns = [
-    path('/register/manager', ListCreateManagerView.as_view(), name='register-manager'),
-    path('/manager/<int:pk>/ban', ManagerBanView.as_view(), name='manager-ban'),
-    path('/manager/<int:pk>/unban', ManagerUnbanView.as_view(), name='manager-unban'),
+    path('/register/manager', ListCreateManagerView.as_view(), name='register_manager'),
+    path('/manager/<int:pk>/ban', ManagerBanView.as_view(), name='manager_ban'),
+    path('/manager/<int:pk>/unban', ManagerUnbanView.as_view(), name='manager_unban'),
 
 ]
--- backend/configs/extra_conf/jwt_conf.py
+++ backend/configs/extra_conf/jwt_conf.py
@@ -1,8 +1,8 @@
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=30),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": True,
