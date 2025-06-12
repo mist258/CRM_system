c368ddd0c2965a61268c46034f37286f3dd0a4a4
mist256 <natalia.kolchuk@gmail.com>
2025-06-12 21:57:46 +0300

    fix url

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import GetMeView, ListCreateManagerView, ManagerBanView, ManagerUnbanView
 
 urlpatterns = [
-    path('/register/manager', ListCreateManagerView.as_view(), name='register_manager'),
+    path('/managers', ListCreateManagerView.as_view(), name='register_manager'),
     path('/manager/<int:pk>/ban', ManagerBanView.as_view(), name='manager_ban'),
     path('/manager/<int:pk>/unban', ManagerUnbanView.as_view(), name='manager_unban'),
     path('/manager/info', GetMeView.as_view(), name='manager_info'),
